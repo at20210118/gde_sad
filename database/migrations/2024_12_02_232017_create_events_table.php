01f56a8536f982d707f55e78b9dbf3ac7ecaf2ad
Aleksandra Trifunovic <at20210118@student.fon.bg.ac.rs>
2024-12-03 15:25:17 +0100

    Kreiranje osnovnih modela

---
+++ database/migrations/2024_12_02_232017_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('place');
+            $table->string('event');
+            $table->string('description');
+            $table->dateTime('event_start');
+            $table->foreignId('category_id');
+            $table->foreignId('location_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
